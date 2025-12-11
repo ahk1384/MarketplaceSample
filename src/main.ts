ddb452fe06b8129bb39b719d44d7f38b95d6840e
AmirHossien Karimi <amirhkz1384@gmail.com>
2025-12-11 21:57:40 +0330

    Remove unnecessary whitespace and add repository URL comment in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
+//git Repository : https://github.com/ahk1384/MarketplaceSample.git
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
